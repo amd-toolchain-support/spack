043764da01c000676d0a23fa45e1760ebba1961b
amd-toolchain-support <toolchainsupport@amd.com>
2021-06-28 02:10:58 -0400

    Updated amdblis recipe to pick following commit id:
    Commit-ID: 2d877199225e3040150580d844ac1a092d472929

--- var/spack/repos/builtin/packages/amdblis/package.py
+++ var/spack/repos/builtin/packages/amdblis/package.py
@@ -21,7 +21,7 @@ class Amdblis(BlisBase):
 
     maintainers = ['amd-toolchain-support']
 
-    version('3.0.1', sha256='33469d8f334e571a2bccb444d55ac11ca9dcd4a01579d1291efef79bc7f14489')
+    version('3.0.1', sha256='31a71cc595da4929cbf30a8d53814a56f77632699a80f1dfcb5818d8d9214e1a')
     version('3.0', sha256='ac848c040cd6c3550fe49148dbdf109216cad72d3235763ee7ee8134e1528517')
     version('2.2', sha256='e1feb60ac919cf6d233c43c424f6a8a11eab2c62c2c6e3f2652c15ee9063c0c9')
 
